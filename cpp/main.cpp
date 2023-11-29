dc0b4cd2b71946f7e6f55ce110df431619d90d2b
Tarek Ziadé <tarek@ziade.org>
2023-11-29 14:37:38 +0100

    added the right file

---
+++ cpp/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include "../output/summary.h" // Include the generated header file
+
+
+int main() {
+    std::ifstream file("sandman.txt");
+
+    if (file.is_open()) {
+        std::string file_content((std::istreambuf_iterator<char>(file)),
+                                 std::istreambuf_iterator<char>());
+
+        const char* input = file_content.c_str();
+
+      char* result = summarize_text(input);
+
+      if (result != nullptr) {
+          std::cout << "Result from Rust function: " << result << std::endl;
+
+          free_memory(result);
+      } else {
+          std::cout << "Error processing the string in Rust." << std::endl;
+      }
+    } else {
+      std::cout << "Unable to open file." << std::endl;
+    }
+    return 0;
+}
+
